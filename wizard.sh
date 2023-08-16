1fc3ca3519441f3985f36371d64c5642231ced40
heschwerdt <henry.schwerdtner@web.de>
2023-08-16 14:28:16 +0200

    better wizard

--- wizard.sh
+++ wizard.sh
@@ -30,7 +30,7 @@ echo -e "${GREEN}Welcome to the Tracking Detector Gym setup wizard!${NC}\n"
 
 admin_api_key=$(prompt_or_generate "Admin API Key" 32)
 admin_password=$(prompt_or_generate "Admin Password" 32)
-minio_private_key=$(prompt_or_generate "MinIO Private Key" 32)
+minio_private_key=$(prompt_or_generate "MinIO Private Key" 16)
 
 # Write to .env file
 cat > .env <<EOL
@@ -53,19 +53,44 @@ ADMIN_USERNAME=admin
 ADMIN_PASSWORD=$admin_password
 EOL
 
-echo -e "\n${GREEN}Setup completed and .env file generated!${NC}\n"
+read -p "${GREEN}Do you want to start in production (y/n):${NC} " start_prod
+if [[ $start_prod == 'y' || $start_prod == 'Y' ]]; then
+    read -p "${GREEN}Enter Domain:${NC} " domain
+    read -p "${GREEN}Enter Email:${NC} " email
+    cat >> .env <<EOL
+DOMAIN=$domain
+EMAIL=$email
+EOL
+    echo -e "\n${GREEN}Setup completed and .env file generated!${NC}\n"
+    read -p "${GREEN}Do you want to start Docker Compose (build and up)? (y/n):${NC} " start_docker
+    if [[ $start_docker == 'y' || $start_docker == 'Y' ]]; then
+      echo -e "${GREEN}Starting Docker Compose...${NC}"
+      sed "s/\$DOMAIN/$domain/g" ./infra/api-gateway/nginx.conf.template > ./infra/api-gateway/nginx.conf
+      docker-compose build
+      docker-compose up -d
+      echo -e "${GREEN}Docker Compose has been started in daemon mode.${NC}"
+    else
+      echo -e "${RED}Exiting without starting Docker Compose.${NC}"
+    fi
+else
+    cat >> .env <<EOL
+DOMAIN=localhost
+EOL
+    echo -e "\n${GREEN}Setup completed and .env file generated!${NC}\n"
+    read -p "${GREEN}Do you want to start Docker Compose (build and up)? (y/n):${NC} " start_docker
+    if [[ $start_docker == 'y' || $start_docker == 'Y' ]]; then
+      echo -e "${GREEN}Starting Docker Compose...${NC}"
+      domain="localhost"
+      sed "s/\$DOMAIN/$domain/g" ./infra/api-gateway/nginx.conf.template > ./infra/api-gateway/nginx.conf
+      docker-compose -f docker-compose.local.yml build
+      docker-compose -f docker-compose.local.yml up -d
+      echo -e "${GREEN}Docker Compose has been started in daemon mode.${NC}"
+    else
+      echo -e "${RED}Exiting without starting Docker Compose.${NC}"
+    fi
+fi
+   
 echo "Generated/Entered values:"
 echo -e "${YELLOW}Admin API Key:${NC} $admin_api_key"
 echo -e "${YELLOW}Admin Password:${NC} $admin_password"
 echo -e "${YELLOW}MinIO Private Key:${NC} $minio_private_key"
-
-read -p "${GREEN}Do you want to start Docker Compose (build and up)? (y/n):${NC} " start_docker
-
-if [[ $start_docker == 'y' || $start_docker == 'Y' ]]; then
-    echo -e "${GREEN}Starting Docker Compose...${NC}"
-    docker-compose build
-    docker-compose up -d
-    echo -e "${GREEN}Docker Compose has been started in daemon mode.${NC}"
-else
-    echo -e "${RED}Exiting without starting Docker Compose.${NC}"
-fi
