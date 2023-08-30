c47777e5700e8380a7eeee06ece5f6aa7a3a01ca
heschwerdt <h.schwerdtner@tu-berlin.de>
2023-08-30 10:32:47 +0200

    Changed batch size

--- python/lib/train.py
+++ python/lib/train.py
@@ -3,4 +3,4 @@ def train_model(modelName: str, dataSetName: str):
     print(f"Training model: {modelName} with data: {dataSetName}")
     for model in get_all_available_models():
         if model.model_name == modelName:
-            model.train(dataSetName, 512, 10)
+            model.train(dataSetName, 128, 10)
